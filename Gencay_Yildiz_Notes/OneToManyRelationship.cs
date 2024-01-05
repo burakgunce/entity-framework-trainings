f5f0eeab0151349bafc3af3e9727e571df03a895
burakgunce <brk.gnc9@hotmail.com>
2024-01-05 19:36:11 +0300

    .

---
+++ Gencay_Yildiz_Notes/OneToManyRelationship.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gencay_Yildiz_Notes
+{
+    internal class OneToManyRelationship
+    {
+        ESirketDbContext context = new();
+
+        #region Default Convention
+        //Default convention yönteminde bire çok ilişkiyi kurarken foreign key kolonuna karşılık gelen bir property tanımlamak mecburiyetinde değilidiz. Eğer tanımlamazsak EF Core bunu kendisi tamamlayacak yok eğer tanımlarsak, tanımladığımızı baz alacaktır.
+        //class Calisan //Dependent Entity
+        //{
+        //    public int Id { get; set; }
+        //    public int DepartmanId { get; set; }
+        //    public string Adi { get; set; }
+
+        //    public Departman Departman { get; set; }
+        //}
+        //class Departman
+        //{
+        //    public int Id { get; set; }
+        //    public string DepartmanAdi { get; set; }
+
+        //    public ICollection<Calisan> Calisanlar { get; set; }
+
+        //}
+        #endregion
+        #region Data Annotations
+        //Default convention yönteminde foreign key kolonuna karşılık gelen property'i tanımladığımızda bu property ismi temel geleneksel entity tanımlama kurallarına uymuyorsa eğer Data Annotations'lar ile müdahalede bulunabiliriz."
+        //class Calisan //Dependent Entity
+        //{
+        //    public int Id { get; set; }
+        //    [ForeignKey(nameof(Departman))]
+        //    public int DId { get; set; }
+
+        //    public string Adi { get; set; }
+
+        //    public Departman Departman { get; set; }
+        //}
+        //class Departman
+        //{
+        //    public int Id { get; set; }
+        //    public string DepartmanAdi { get; set; }
+
+        //    public ICollection<Calisan> Calisanlar { get; set; }
+
+        //}
+        #endregion
+        #region Fluent API
+        class Calisan //Dependent Entity
+        {
+            public int Id { get; set; }
+            public int DId { get; set; }
+            public string Adi { get; set; }
+
+            public Departman Departman { get; set; }
+        }
+        class Departman
+        {
+            public int Id { get; set; }
+            public string DepartmanAdi { get; set; }
+
+            public ICollection<Calisan> Calisanlar { get; set; }
+
+        }
+        #endregion
+
+
+        class ESirketDbContext : DbContext
+        {
+            public DbSet<Calisan> Calisanlar { get; set; }
+            public DbSet<Departman> Departmanlar { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ESirketDB;User ID=SA;Password=1q2w3e4r+!");
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Calisan>()
+                    .HasOne(c => c.Departman)
+                    .WithMany(d => d.Calisanlar)
+                    .HasForeignKey(c => c.DId);
+            }
+        }
+    }
+}
